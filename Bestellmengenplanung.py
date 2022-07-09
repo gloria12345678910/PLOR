67fc2a063bf11d8d54c483116624ad2815c735de
Gloria <gloria.hh2001@gmail.com>
2022-07-09 18:38:34 +0200

    Bestellmengenplanung ohne Rabatt

--- Bestellmengenplanung.py
+++ Bestellmengenplanung.py
@@ -2,27 +2,62 @@ import math
 
 
 class Bestellmengenplanung:
-
-    def __init__(self, nachfrage, kostenProBestellung, lagerkostenProMengeundZeiteinheit):
-        self.lagerkostenProMengeundZeiteinheit = lagerkostenProMengeundZeiteinheit
-        self.kostenProBestellung = kostenProBestellung
+    def __init__(self, nachfrage, bestellkosten, lagerkosten):
+        #lagerkosten= h
+        self.lagerkosten = lagerkosten
+        #bestellkosten= f
+        self.bestellkosten = bestellkosten
+        #nachfrage= d
         self.nachfrage = nachfrage
+        self.calculateAll()
 
-    def optBestellmenge(self):
-        return math.sqrt((2 * self.nachfrage * self.kostenProBestellung) / self.lagerkostenProMengeundZeiteinheit)
+    def optBestellmenge(self, printOut = 1):
+        erg= math.sqrt((2 * self.nachfrage * self.bestellkosten) / self.lagerkosten)
+        if printOut:
+            print(f'q*={erg}')
+        return erg
 
-    technology
+    def anzBestellungen(self, printOut=1):
+        anzBestellungen= self.nachfrage/self.optBestellmenge(printOut=0)
+        if printOut:
+            print(f'anzahl Bestellungen={anzBestellungen}')
+        return anzBestellungen
+
     def anzBestellungenProJahr(self):
-        return self.nachfrage/self.optBestellmenge()
+        anzBestellungenProJahr = self.anzBestellungen(printOut=0)*356
+        print(f'anz Bestellungen Pro Jahr={anzBestellungenProJahr}')
+        return anzBestellungenProJahr
 
     def zyklusdauer(self):
-        return self.optBestellmenge()/self.nachfrage
+        #zyklusdauer= Zeit zwischen den Bestellungen
+        zyklusdauer=self.optBestellmenge(printOut=0)/self.nachfrage
+        print(f'zyklusdauer ( Zeit zwischen den Bestellungen) ={zyklusdauer}')
+        return zyklusdauer
 
-    def bestellkosten(self):
-        return self.nachfrage/self.optBestellmenge()*self.kostenProBestellung
+    def jaehrlicheBestellkosten(self):
+        jaehrlicheBestellkosten=self.nachfrage/self.optBestellmenge(printOut=0)*self.bestellkosten
+        print(f'jearliche Bestellkosten={jaehrlicheBestellkosten}')
+        return jaehrlicheBestellkosten
 
     def jaehrlicheLagerkosten(self):
-        return 0.5*self.lagerkostenProMengeundZeiteinheit*self.optBestellmenge()
+        jearlicheLagerkosten= 0.5 * self.lagerkosten * self.optBestellmenge(printOut=0)
+        print(f'jearlicheLagerkoster ={jearlicheLagerkosten}')
+        return jearlicheLagerkosten
 
-    def zielfunktionGesamtkosten(self):
-        return self.nachfrage/self.optBestellmenge()*self.kostenProBestellung+0.5*self.lagerkostenProMengeundZeiteinheit*self.kostenProBestellung
+    def gesamtkosten(self):
+        gesamtkosten=(self.nachfrage / self.optBestellmenge(printOut=0)) * self.bestellkosten + 0.5 * self.lagerkosten * self.optBestellmenge()
+        print(f'gesamtkosten={gesamtkosten}')
+        return gesamtkosten
+
+    def calculateAll(self):
+        self.optBestellmenge()
+        self.anzBestellungen()
+        self.anzBestellungenProJahr()
+        self.zyklusdauer()
+        self.jaehrlicheBestellkosten()
+        self.jaehrlicheLagerkosten()
+        self.gesamtkosten()
+
+
+if __name__ == '__main__':
+    bestellmengenplanung = Bestellmengenplanung(1500,15,0.15)
