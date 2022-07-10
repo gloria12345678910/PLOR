4b42ccf9f0b12ba34a794677eb97cb3f8268c03f
Philipp Grill <philipp.grill@freenet.de>
2022-07-10 14:43:45 +0200

    fertig

--- BestellmengenplanungRabatt.py
+++ BestellmengenplanungRabatt.py
@@ -1,6 +1,7 @@
 import numpy as np
-
+from utils import runden
 from Bestellmengenplanung import Bestellmengenplanung
+
 
 
 class Preisliste:
@@ -33,42 +34,73 @@ class BestellmengenplanungRabatt:
     def lagerkosten(self, printOut=1):
         h = np.asarray([])
         for i in range(len(self.preisliste.preisProME)):
-            h = np.append(h, self.prozentanteilEinkaufspreises * self.preisliste.preisProME[i])
+            h = np.append(h, runden(self.prozentanteilEinkaufspreises * self.preisliste.preisProME[i],zwischenergebnis=True))
             if printOut:
-                print(f'h{i + 1} = {round(h[i] * 10000) / 10000} in dem Intervall {self.preisliste.auftragsmenge[i]}')
+                print(f'h{i + 1} = {runden(h[i])} in dem Intervall {self.preisliste.auftragsmenge[i]}')
             i += 1
         return h
 
-    def optBestellPolitik(self):
+    def optBestellPolitik(self, printOut=1):
         q = np.asarray([])
         i = 0
         for lagerkosten in self.lagerkosten(printOut=0):
             qi = Bestellmengenplanung(self.nachfrage, self.bestellkosten, lagerkosten).optBestellmenge(printOut=0)
-            print((f'q{i + 1} = {round(qi * 10000) / 10000} in dem Intervall {self.preisliste.auftragsmenge[i]}'))
-            q = np.append(q, qi)
+            if printOut:
+                print((f'q{i + 1} = {runden(qi)} in dem Intervall {self.preisliste.auftragsmenge[i]}'))
+            q = np.append(q, runden(qi,True))
             i += 1
         return q
 
-    def zulaessigkeit(self):
+    def zulaessigkeit(self, printOut=1):
         i = 0
         optBestellmenge = np.asarray([])
-        for q in self.optBestellPolitik():
+        for q in self.optBestellPolitik(printOut=0):
             if q > self.preisliste.obereSchranke(i):
-                print(
-                    f'q{i + 1}*={q} ist keine zulässige Lösung, da der Wert q{i + 1}*={q} nicht im Intervall{self.preisliste.auftragsmenge[i]} liegt.')
+                optBestellmenge = np.append(optBestellmenge,-1)
+                if printOut:
+                    print(
+                        f'q{i + 1}*={runden(q)} ist keine zulässige Lösung, da der Wert q{i + 1}*={runden(q)} nicht im Intervall{self.preisliste.auftragsmenge[i]} liegt.')
             elif (q >= self.preisliste.untereSchranke(i)) and (q <= self.preisliste.obereSchranke(i)):
-                optBestellmenge = np.append(optBestellmenge, q)
-                print(
-                    f'q{i + 1}*={q} ist eine zulässige Lösung da der Wert q{i + 1}*={q} im Intervall{self.preisliste.auftragsmenge[i]} liegt.')
+                optBestellmenge = np.append(optBestellmenge, runden(q,True))
+                if printOut:
+                    print(
+                        f'q{i + 1}*={runden(q)} ist eine zulässige Lösung da der Wert q{i + 1}*={runden(q)} im Intervall{self.preisliste.auftragsmenge[i]} liegt.')
             else:
-                print(
-                    f'q{i + 1}*={q} ist eine zulässige Lösung, da der Wert q{i + 1}*={q} kleiner als die untere Schranke ist. Damit nimmt der neue Wert q{i + 1}*={self.preisliste.untereSchranke(i)} an')
-                optBestellmenge = np.append(optBestellmenge, self.preisliste.untereSchranke(i))
+                if printOut:
+                    print(
+                        f'q{i + 1}*={runden(q)} ist eine zulässige Lösung, da der Wert q{i + 1}*={runden(q)} kleiner als die untere Schranke ist. Damit nimmt der neue Wert q{i + 1}*={self.preisliste.untereSchranke(i)} an')
+                optBestellmenge = np.append(optBestellmenge, runden(self.preisliste.untereSchranke(i),True))
             i += 1
         return optBestellmenge
 
+    def gesamtkosten(self, printOut=1):
+        kosten = np.asarray([])
+        optBestellmenge = self.zulaessigkeit(printOut=0)
+        for i in range(len(optBestellmenge)):
+            if round(optBestellmenge[i]) ==-1:
+                if printOut:
+                    print(f'F(q{i+1}*) entfällt')
+                kosten = np.append(kosten, 1000000000000)
+            else:
+                erg = (self.nachfrage / optBestellmenge[i] )* self.bestellkosten + 0.5 * self.lagerkosten(printOut=0)[i] * optBestellmenge[i] + self.nachfrage * self.preisliste.preisProME[i]
+                kosten = np.append(kosten, runden(erg, True))
+                if printOut:
+                    print(f'F(q{i+1}*)={runden(kosten[i])}')
+        return kosten
+
+    def endErg(self):
+        minGesamtkosten = np.min(self.gesamtkosten(printOut=0))
+        i = np.argmin(self.gesamtkosten(printOut=0))
+        optBestellmenge = self.zulaessigkeit(printOut=0)[i]
+        print(f'endgültig optimale Bestellmenge: q{i}*={optBestellmenge}')
+        print(f'dazugehörige minimale Gesammtkosten: {runden(minGesamtkosten)}')
+
+        anzBestellungen = runden(self.nachfrage/optBestellmenge)
+        dauerZwischenZweiBestellungen = runden(optBestellmenge/self.nachfrage)
+        print(f'anzahl der Bestellungen: {anzBestellungen}')
+        print(f'dauer zwischen zwei Bestellungen: {dauerZwischenZweiBestellungen}')
 
 if __name__ == '__main__':
     preisliste = Preisliste([(1, 9999), (10000, 19999), (20000,)], [0.45, 0.38, 0.35])
     bestellmengenplanungRabatt = BestellmengenplanungRabatt(90000, 90, preisliste, 0.2)
-    bestellmengenplanungRabatt.zulaessigkeit()
+    bestellmengenplanungRabatt.endErg()
